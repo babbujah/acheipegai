82bd8e9f54781f29655f07b3878f4b765f7d2dce
Bruno Lopes <bczion@hotmail.com>
2023-04-12 17:31:43 -0300

    Inclusão dos models e listagem de Lojas.

---
+++ admin/app/model/loja/Loja.php
@@ -0,0 +1,26 @@
+<?php
+/**
+* Loja Active Record
+* @author brunosilva
+**/
+class Loja extends TRecord{
+    const TABLENAME = 'loja';
+    const PRIMARYKEY = 'id';
+    const IDPOLICE = 'serial';
+    
+    private $nome;
+    private $link_afiliado;
+    private $logo;
+    
+    /**
+    * Método construtor
+    **/
+    public function __construct($id = NULL, $callObjectLoad = TRUE){
+        parent::__construct($id, $callObjectLoad);
+        
+        parent::addAttribute('nome');
+        parent::addAttribute('link_afiliado');
+        parent::addAttribute('logo');
+        
+    }
+}
