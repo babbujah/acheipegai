0d03a0c6920771953653ad50c2398e08c91cdbf1
Daniel Franklin <dnlfranklin.ls@gmail.com>
2023-04-17 16:04:42 -0300

    Page Home

---
+++ pages/404.php
@@ -0,0 +1,10 @@
+<section class="http-error">
+    <div class="row justify-content-center py-3">
+        <div class="col-md-12 text-center">
+            <div class="http-error-main">
+                <h2>404!</h2>
+                <p>Desculpe-nos! A página que você está buscando não existe.</p>
+            </div>
+        </div>
+    </div>
+</section>
