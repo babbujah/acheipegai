7cd78c8096436173eba570462647e78fa4b182ab
Bruno Lopes <bczion@hotmail.com>
2023-04-19 13:13:09 -0300

    Ajuste da exbibição da imagem.

--- admin/app/service/FormatarDados.php
+++ admin/app/service/FormatarDados.php
@@ -29,10 +29,9 @@ class FormatarDados{
      * @author brunosilva 
     **/
     public static function formatarImagem( $imagem, $object, $row ){
-        var_dump(file_exists('./'.$imagem));
-        var_dump('./'.$imagem);
+        
         $capturaImagem = $imagem;
-        if( empty($capturaImagem) or file_exists('./'.$capturaImagem) ){
+        if( empty($capturaImagem) ){
             //return "";
             //return URL_BASE.'img/produtos/noimage.png';
             $capturaImagem = 'img/produtos/noimage.png';
@@ -42,6 +41,7 @@ class FormatarDados{
         //$imagemFormatada = new TImage( URL_BASE.$imagem );
         $imagemFormatada = new TImage( URL_BASE.$capturaImagem );
         $imagemFormatada->style = 'max-width: 140px';
+        $imagemFormatada->onerror = "this.onerror=null;this.src='app/images/noimage.png';";
         
         return $imagemFormatada;
     }
